3fc95e4004a2ed0e99fd5b1126cdcf52dcba59d6
Dario Vladovic <d.vladimyr@gmail.com>
2020-02-07 22:40:34 +0100

    Update test suite
    - using async/await

--- test/pgpsearch.spec.js
+++ test/pgpsearch.spec.js
@@ -1,79 +1,74 @@
 'use strict';
 
-var openpgp = require('openpgp');
-var expect = require('chai').expect;
-var pgpsearch = require('../');
-var request = require('request');
+global.URL = global.URL = require('url').URL;
 
-var email = 'xdamman@gmail.com';
-var fingerprint = '44A7D05FF1F6D0B80F7915A614261B13FD430CDC';
+const { expect } = require('chai');
+const openpgp = require('openpgp');
+const pgpsearch = require('../');
+const { promisify } = require('util');
+const request = promisify(require('request'));
+const url = require('native-url');
+
+pgpsearch.get = promisify(pgpsearch.get);
+pgpsearch.index = promisify(pgpsearch.index);
+
+const email = 'xdamman@gmail.com';
+const fingerprint = '44A7D05FF1F6D0B80F7915A614261B13FD430CDC';
 
 describe('pgp', function () {
-  it('All the servers should work', function (done) {
-    var n = pgpsearch.keyServers.length;
-    var complete = 0;
+  it('All the servers should work', () => {
+    const query = {
+      search: email,
+      op: 'index',
+      fingerprint: 'on',
+      options: 'mr'
+    };
+    return Promise.all(pgpsearch.keyServers.map(async server => {
+      const lookupUrl = url.format({
+        protocol: 'https:',
+        host: server,
+        pathname: '/pks/lookup',
+        query
+      });
+      const resp = await request(lookupUrl);
+      if (resp.statusCode !== 200) {
+        throw new Error(`HTTPError: Response code ${resp.statusCode} (${resp.statusMessage})`);
+      }
+    }));
+  });
 
-    function response (err, res, body) {
-      if (err) {
-        done(err);
-      }
-      if (res.statusCode !== 200) {
-        done(new Error('Error: Status: ' + res.statusCode));
-      }
-      complete = complete + 1;
-      if (complete === n) {
-        done();
-      }
+  it('Finds the public PGP key for an email address', async () => {
+    const keys = await pgpsearch.index(email);
+    expect(keys.length).to.equal(1);
+    expect(keys[0].fingerprint).to.equal(fingerprint);
+  });
+
+  it("Returns an error if can't find a PGP key", async () => {
+    try {
+      await pgpsearch.index('notfound' + email);
+    } catch (err) {
+      expect(err).to.exist;
     }
+  });
 
-    for (var i = 0; i < pgpsearch.keyServers.length; i++) {
-      var requestOptions = {};
-      requestOptions.url = 'https://' + pgpsearch.keyServers[i] + '/pks/lookup?search=' + encodeURIComponent(email) + '&op=index&fingerprint=on&options=mr';
-      request(requestOptions, response);
+  it('Returns multiple keys', async () => {
+    const email = 'glenn.greenwald@theintercept.com';
+    const keys = await pgpsearch.index(email);
+    expect(keys.length > 1).to.be.true;
+  });
+
+  it('Gets the PGP public key given a fingerprint', async () => {
+    const pgp = await pgpsearch.get(fingerprint);
+    const publicKey = openpgp.key.readArmored(pgp);
+    const publicKey_fingerprint = publicKey.keys[0].primaryKey.getFingerprint().toUpperCase();
+    expect(publicKey_fingerprint).to.equal(fingerprint);
+  });
+
+  it("Returns an error if it can't find a PGP key for a given fingerprint", async () => {
+    try {
+      await pgpsearch.get(fingerprint.replace(/[0-9]/g, '0'));
+    } catch (err) {
+      expect(err).to.exist;
     }
-  });
-
-  it('Finds the public PGP key for an email address', function (done) {
-    pgpsearch.index(email, function (e, keys) {
-      expect(e).to.not.exist;
-      expect(keys.length).to.equal(1);
-      expect(keys[0].fingerprint).to.equal(fingerprint);
-      done();
-    });
-  });
-
-  it("Returns an error if can't find a PGP key", function (done) {
-    pgpsearch.index('notfound' + email, function (e, res) {
-      expect(e).to.exist;
-      done();
-    });
-  });
-
-  it('Returns multiple keys', function (done) {
-    var email = 'glenn.greenwald@theintercept.com';
-    pgpsearch.index(email, function (e, keys) {
-      expect(e).to.not.exist;
-      expect(keys.length > 1).to.be.true;
-      done();
-    });
-  });
-
-  it('Gets the PGP public key given a fingerprint', function (done) {
-    pgpsearch.get(fingerprint, function (e, pgp) {
-      expect(e).to.not.exist;
-
-      var publicKey = openpgp.key.readArmored(pgp);
-      var publicKey_fingerprint = publicKey.keys[0].primaryKey.getFingerprint().toUpperCase();
-      expect(publicKey_fingerprint).to.equal(fingerprint);
-
-      done();
-    });
-  });
-
-  it("Returns an error if it can't find a PGP key for a given fingerprint", function (done) {
-    pgpsearch.get(fingerprint.replace(/[0-9]/g, '0'), function (e, pgp) {
-      expect(e).to.exist;
-      done();
-    });
   });
 });
